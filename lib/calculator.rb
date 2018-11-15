15e5080f1d2507a81e2968cb5a73b90faf5a44fe
Max Stevenson <maxstevenson@msn.com>
2018-11-15 19:43:37 +0000

    - Constructed a number of feature tests to drive development of central logic. - Constrcuted a Calculator class with a check_birthday method that takes a day a month. - All tests work as expected.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,10 +3,20 @@ require 'time'
 class Calculator
 	def check_birthday (day, month)
 		current_date = Time.now
+	  year = Time.now.year
+	  
+	  if DateTime.parse("#{day}-#{month}-#{year}").month < current_date.month && 
+	  	DateTime.parse("#{day}-#{month}-#{year}").day < current_date.day
+	    year += 1
+	  else
+	    year
+	  end
+
 		if day == current_date.day && month == current_date.strftime("%B")
 			true
 		else
-			false
+			next_birthday = DateTime.parse("#{day}#{month}#{year}")
+			(next_birthday - Date.today).to_i.abs
 		end
-	end
+	end		
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,5 +9,13 @@ describe 'Calculator' do
 			month = test_date.strftime("%B")
 			expect(cal.check_birthday(day, month)).to eq(true)
 		end
+
+		it "Should return the number of days until next birthday" do
+			cal = Calculator.new
+			test_date = Time.now
+			day = test_date.day + 5
+			month = test_date.strftime("%B")
+			expect(cal.check_birthday(day, month)).to eq(5)
+		end
 	end
 end
